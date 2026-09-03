e4f659b92f83498fad6b074fdad20538e2360a89
AlexD042 <drews.alex42@gmail.com>
2026-09-03 11:14:39 -0500

    First draft completed.

--- StringCalculatorLab/main.cpp
+++ StringCalculatorLab/main.cpp
@@ -7,17 +7,22 @@ void test_stringCalculator();
 
 int main() {
 	test_stringCalculator();
-
 	return 0;
 }
 
 int stringCalculator(std::string str) {
 	int total = 0;
 	std::string tempStr = "";
-	for (int i = 0; i < str.size(); i++) {
-		if (str[i] != ',' && str[i] != ';') {
+
+	for (int i = 0; i <= str.size(); i++) {
+		if (str[i] == '\0' && tempStr.size() != 0) {
+			total += stoi(tempStr);
+		}
+		else if (str[i] != ',' && str[i] != ';') {
 			tempStr += str[i];
-			total += std::stoi(tempStr);
+		}
+		else {
+			total += stoi(tempStr);
 			tempStr = "";
 		}
 	}
@@ -31,6 +36,9 @@ void test_stringCalculator() {
 	assert(stringCalculator("1") == 1);
 	assert(stringCalculator("1,2") == 3);
 	assert(stringCalculator("4,5,6") == 15);
+	assert(stringCalculator("-1,1") == 0);
+	assert(stringCalculator("-9,1") == -8);
+	assert(stringCalculator("16,24") == 40);
 
 	std::cout << "test_stringCalculator PASSED";
 }
